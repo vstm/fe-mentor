4999393a95c650e132e9e27917a4427423ecc2cf
Stefan Vetsch <s@svetsch.ch>
2024-08-19 18:59:43 +0200

    Add & init product-preview-card-component-main challenge

---
+++ product-preview-card-component-main/tailwind.config.js
@@ -0,0 +1,12 @@
+// const defaultTheme = require('tailwindcss/defaultTheme')
+// const plugin = require('tailwindcss/plugin')
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [`index.html`],
+    theme: {
+        extend: {},
+    },
+    plugins: [
+    ],
+}
